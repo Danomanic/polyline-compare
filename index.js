572e67e64d9e4f955fa7723f9812910b04b61443
Danomanic <daniel.pomfret@googlemail.com>
2022-12-07 22:03:59 +0000

    Loop through hills, check against polyline

---
+++ index.js
@@ -0,0 +1,25 @@
+const polyline = require('@mapbox/polyline');
+const { PolyUtil } = require('node-geometry-library');
+
+// Get data from json file
+const hills = require('./hills.json');
+
+// Get the encoded polyline from the text file
+const encodedPolyline = require('fs').readFileSync('./encodedPolyline.txt', 'utf8');
+
+const decodedPolyline = PolyUtil.decode(encodedPolyline);
+
+// Loop through each hill
+hills.forEach(hill => {
+    // Get the longitude and latitude from the hill
+    const { longitude, latitude } = hill;
+
+    // Check isLocationOnPath
+    const isLocationOnPath = PolyUtil.isLocationOnPath({ lat: latitude, lng: longitude }, decodedPolyline, 250, false);
+
+    if (isLocationOnPath) {
+        console.log(`Hill: ${hill.name} is on the edge: ${isLocationOnPath} `);
+        console.log(hill.type)
+    }
+
+});
